84aa90b7ee94e639d35758559e0d03459c72a843
DesignRia <sairusairu@naver.com>
2018-03-20 17:21:29 +0900

    모바일 메뉴 제어를 위한 버튼 완성

--- JS/example-12.js
+++ JS/example-12.js
@@ -1,6 +1,18 @@
 $(document).ready(function(){
     // console.log('OK');
-    $('.menubar').on('click',function(){
-        $('.menu').toggleClass('menu-act');
+    var menubar = $('.menubar, .btn-menubar');
+    var bar = $('.menubar');
+    var btn = $('.btn-menubar');
+    var menu = $('.menu');
+
+    menubar.on('click',function(){
+        menu.toggleClass('menu-act'); //Class라고 알려줬으므로 . 삭제
+        bar.toggleClass('menubar-act');
+        if(menu.hasClass('menu-act')){
+            btn.text('메뉴 닫기');
+        } else {
+            btn.text('메뉴 열기');
+        }
     });
+    // if문은 끝날때 ;을 넣지 않는다.
 });
